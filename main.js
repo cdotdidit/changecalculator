29e66770dc409ef0baed2f9640b02d400c38f14a
cash <cdotcomma@gmail.com>
2019-06-17 14:38:18 -0700

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // Write your JavaScript here
+
+function calculateChange(amountDue, amountReceived){
+    
+    var amountDue = document.getElementById("amount-due").value;
+    var amountReceived = document.getElementById("amount-received").value;
+    var result = amountReceived - amountDue;
+    console.log(result);
+
+    return result;
+};
+
+
+  function handleClickEvent() {
+    var amountDue = document.getElementById("amount-due").value;
+    var amountReceived = document.getElementById("amount-received").value;
+    var result = calculateChange(amountDue, amountReceived);
+
+        document.getElementById("calculate-change").innerHTML = "Your change is: $" + result+ ".";
+    }
